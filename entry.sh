63d8fd91006550e8c3f610140fb99a4b8d0b6d39
strikergoutham <strikergoutham@gmail.com>
2020-07-16 07:39:24 +0530

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 echo running gitleaks "$(gitleaks --version) with the following command👇";
-$MASTER_COMMIT = git ls-remote $CIRCLE_REPOSITORY_URL refs/heads/master;
-echo $MASTER_COMMIT;
+git ls-remote $CIRCLE_REPOSITORY_URL refs/heads/master;
